1276b48471829faf89a49eb68ce50743587ce38f
wafaaobeido <wafaa-obeido@hotmail.com>
2025-02-11 22:23:54 +0100

    feat(holiday ms): setup the Holiday microservice

---
+++ backend/holiday-service/src/main/java/com/holiday_tracker/holiday_service/HolidayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.holiday_tracker.holiday_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayServiceApplication.class, args);
+	}
+
+}
